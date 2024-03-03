03ed183a90962d2be514fe54b565a4a7bdd8be26
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-03 11:52:03 +0530

    Create powerOf_2_nums.cpp

---
+++ Maths/powerOf_2_nums.cpp
@@ -0,0 +1,11 @@
+int power(int num1 , int num2){
+  int ans = 1;
+  for(int i=1;i<=num2;i++){
+   ans = ans * mum1;
+  }
+return ans;
+}
+
+// 2^3 = 8      --> process :- 1*2 = 2
+                          //   2*2 = 4
+                          //   4*2 = 8
