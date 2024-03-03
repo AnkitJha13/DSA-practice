004224cdbb5c282b298743a6e5099ad6db31f009
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-03 11:43:02 +0530

    Update reverse_integer.cpp

--- Maths/reverse_integer.cpp
+++ Maths/reverse_integer.cpp
@@ -18,3 +18,18 @@ public:
         return ans;
     }
 };
+
+
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
