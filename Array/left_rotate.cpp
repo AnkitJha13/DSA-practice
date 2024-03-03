5c69d703e746c7546ff8bceab1d89b8d395e37fe
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-03 13:35:17 +0530

    Update left_rotate.cpp

--- Array/left_rotate.cpp
+++ Array/left_rotate.cpp
@@ -6,3 +6,11 @@ void rotateArr(int arr[], int d, int n)
     reverse(arr + d, arr + n);
     reverse(arr, arr + n);
 }
+
+
+// Input:
+// N = 5, D = 2
+// arr[] = {1,2,3,4,5}
+// Output: 3 4 5 1 2
+// Explanation: 1 2 3 4 5  when rotated
+// by 2 elements, it becomes 3 4 5 1 2.
