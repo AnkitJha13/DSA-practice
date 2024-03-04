64cddfae89a7ad33f5e66dfcb5f679d3db130ed1
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-04 23:36:42 +0530

    Create Anagram.cpp

---
+++ Strings/Anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i] != t[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+  
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
