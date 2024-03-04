bfe891a517f24e860ecb2068b3d45d3e76219c3b
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-04 08:06:44 +0530

    Create Roman_to_integer.cpp

---
+++ Strings/Roman_to_integer.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m;
+        
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]){
+                ans -= m[s[i]];
+            }
+            else{
+                ans += m[s[i]];
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
